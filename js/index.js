cc7638800682d6eda25507412ea3a19195cd34ea
Anton Sadovskiy <antonsadovskiy6@gmail.com>
2022-12-11 13:22:26 +0300

    added functions to display numbers on the screen

---
+++ js/index.js
@@ -0,0 +1,59 @@
+let arrayNums = ''
+
+function clearAll(){
+    document.getElementById('result').innerHTML = '0';
+    arrayNums = ''
+}
+
+// function typeNum(){
+//     let num = document.getElementById('one').value
+//     arrayNums += num
+//     document.getElementById('result').innerHTML = arrayNums
+// }
+
+function typeOne(){
+    let num = document.getElementById('one').value
+    arrayNums += num
+    document.getElementById('result').innerHTML = arrayNums
+}
+
+function typeTwo(){
+    let num = document.getElementById('two').value
+    arrayNums += num
+    document.getElementById('result').innerHTML = arrayNums
+}
+function typeThree(){
+    let num = document.getElementById('three').value
+    arrayNums += num
+    document.getElementById('result').innerHTML = arrayNums
+}
+function typeFour(){
+    let num = document.getElementById('four').value
+    arrayNums += num
+    document.getElementById('result').innerHTML = arrayNums
+}
+function typeFive(){
+    let num = document.getElementById('five').value
+    arrayNums += num
+    document.getElementById('result').innerHTML = arrayNums
+}
+function typeSix(){
+    let num = document.getElementById('six').value
+    arrayNums += num
+    document.getElementById('result').innerHTML = arrayNums
+}
+function typeSeven(){
+    let num = document.getElementById('seven').value
+    arrayNums += num
+    document.getElementById('result').innerHTML = arrayNums
+}
+function typeEight(){
+    let num = document.getElementById('eight').value
+    arrayNums += num
+    document.getElementById('result').innerHTML = arrayNums
+}
+function typeNine(){
+    let num = document.getElementById('nine').value
+    arrayNums += num
+    document.getElementById('result').innerHTML = arrayNums
+}
